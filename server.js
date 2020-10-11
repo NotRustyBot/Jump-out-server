3dfa2d30d5fcfe2ce571d39f4d7d3b1a81facedf
Matouš Marek <mylapqn312@gmail.com>
2020-10-11 16:31:09 +0200

    f

---
+++ server.js
@@ -0,0 +1,23 @@
+//#region INIT
+var http = require('http');
+var server = http.createServer(function (request, response) {
+});
+var WebSocketServer = require('ws').Server;
+var port = process.env.PORT;
+server.listen(port, function () {
+  console.log((new Date()) + " WS Server is listening on port " + port);
+});
+
+/*wsServer = new WebSocketServer({
+  httpServer: server,
+  keepaliveInterval: 5000,
+  keepaliveGracePeriod: 1000,
+  closeTimeout: 1000
+});*/
+wsServer = new WebSocketServer({ server });
+wsServer.on('connection', onConnection);
+//#endregion
+
+function onConnection(connection){
+    console.log(connection);
+}
