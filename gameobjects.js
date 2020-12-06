d7e0e9c2595c5a6a6ed40b2a2b67d7f56acd36af
Andrej Karovin <andrej.karovin2000@gmail.com>
2020-12-06 14:46:21 +0100

    HOLOUBCI

--- gameobjects.js
+++ gameobjects.js
@@ -574,6 +574,7 @@ function Mobile(x, y, type) {
     }
 }
 
+console.log(process.env.HOLOUBCI);
 for (let index = 0; index < process.env.HOLOUBCI; index++) {
     let m1 = new Mobile(Universe.size * Area.size / 2 + 2000, Universe.size * Area.size / 2, 3);
     m1.collider.push(new Shape().circle(0, 0, 125));
