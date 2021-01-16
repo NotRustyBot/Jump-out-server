701b25d54498e59571c2a05d612a73f138fdea7b
Andrej Karovin <andrej.karovin2000@gmail.com>
2021-01-16 19:41:58 +0100

    Universe.gasChange = [];

--- server.js
+++ server.js
@@ -187,6 +187,8 @@ function updateMessage() {
         Universe.gasChange.forEach(e => {
             view.serialize(e, Datagrams.GasUpdate);
         });
+        
+        Universe.gasChange = [];
     }
 
     return view;
